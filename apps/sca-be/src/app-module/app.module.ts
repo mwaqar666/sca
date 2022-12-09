54a3df7c4b13dda3e26fb066522241cc3c708aa9
Muhammad Waqar <mwaqar666@yahoo.com>
2022-12-09 19:43:14 +0500

    Nrwl upgraded to 15.3.0

--- apps/sca-be/src/app-module/app.module.ts
+++ apps/sca-be/src/app-module/app.module.ts
@@ -1,10 +1,10 @@
-import { ControlDbServiceConfig, ControlServiceConfig } from "~/app-module/config/control-config";
-import * as Services from "~/app-module/services";
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@sca/config";
 import { DbModule } from "@sca/db";
 import { RedisModule } from "@sca/redis";
 import { BaseModule, ControlFlowModule } from "@sca/utils";
+import { ControlDbServiceConfig, ControlServiceConfig } from "~/app-module/config/control-config";
+import * as Services from "~/app-module/services";
 
 @Module({
 	imports: [ConfigModule, RedisModule, DbModule, ControlFlowModule.forRoot(ControlServiceConfig, ControlDbServiceConfig)],
