68bb993eac42dc6f180da22dfe9e7532bed39087
Muhammad Waqar <mwaqar666@yahoo.com>
2022-12-13 15:06:43 +0500

    Packages updated. Task executors simplified.

--- libs/backend/security/src/security.module.ts
+++ libs/backend/security/src/security.module.ts
@@ -1,11 +1,10 @@
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
-import { ConfigModule } from "@sca/config";
 import { BaseModule } from "@sca/utils";
 import * as Services from "./services";
 
 @Module({
-	imports: [ConfigModule, JwtModule],
+	imports: [JwtModule],
 	providers: [...Object.values(Services)],
 	exports: [...Object.values(Services)],
 })
